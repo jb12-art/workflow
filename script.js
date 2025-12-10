b751632fe55df538e573ee8465de2c6eb37da26b
jb12-art <jbqw1@msn.com>
2025-12-10 21:55:09 +0200

    .prettierrc, eslint

---
+++ script.js
@@ -0,0 +1,9 @@
+// test file
+function v(x, c) {
+  const result = x + c;
+  const label = "ddd";
+}
+
+// Brief
+// 3. Set Up Development Tools: 
+// 3.3 Set up commit hooks to ensure staged code is checked before being committed. Format HTML files and format and lint JavaScript files.
